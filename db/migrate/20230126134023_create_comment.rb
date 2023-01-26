5d6faf75f619a376d7fa0143ebedb7e386c68684
Olivier-Kango <olivierkango@gmail.com>
2023-01-26 12:24:40 -0800

    Correct authorId and postId

--- db/migrate/20230126134023_create_comment.rb
+++ db/migrate/20230126134023_create_comment.rb
@@ -1,8 +1,8 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :authorld
-      t.text :postld
+      t.integer :authorId
+      t.integer :postId
       t.text :text
 
       t.timestamps
