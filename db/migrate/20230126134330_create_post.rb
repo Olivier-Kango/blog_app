236bf7948c74955dcd0b768dbd250ecb948137fa
Olivier-Kango <olivierkango@gmail.com>
2023-01-26 12:24:50 -0800

    Change authorId

--- db/migrate/20230126134330_create_post.rb
+++ db/migrate/20230126134330_create_post.rb
@@ -1,7 +1,7 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :authorld
+      t.integer :authorId
       t.string :title
       t.text :text
       t.integer :comments_counter
