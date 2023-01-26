7d2b2cc76dc1f5642f99f278c6a2f7c0ad6d60c1
Olivier-Kango <olivierkango@gmail.com>
2023-01-26 12:25:10 -0800

    Change authorId and postId to integer

--- db/migrate/20230126134803_create_like.rb
+++ db/migrate/20230126134803_create_like.rb
@@ -1,8 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.string :authorld
-      t.text :postld
+      t.integer :authorId
+      t.integer :postId
 
       t.timestamps
     end
