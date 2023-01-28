42f005d00c40f599b493b81063ba12f74755ccd4
Olivier-Kango <olivierkango@gmail.com>
2023-01-28 10:31:36 -0800

    Generate class Like

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :increment_likes_counter
+
+  def increment_likes_counter
+    posts.increment!(:likes_counter)
+  end
+end
