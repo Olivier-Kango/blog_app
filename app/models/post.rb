1f26fe739b1ea45316120d84eed190addbb5ec5c
Olivier-Kango <olivierkango@gmail.com>
2023-01-28 10:31:49 -0800

    Generate class Post

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :increment_posts_counter
+
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comment_counter
+    comments.order(created_at: :desc).limit(5)
+  end
+end
