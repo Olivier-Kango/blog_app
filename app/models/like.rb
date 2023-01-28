b3050149704b99fb42482153edbb333e4e45ba23
Olivier-Kango <olivierkango@gmail.com>
2023-01-28 11:17:36 -0800

    last corrections

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,6 +5,6 @@ class Like < ApplicationRecord
   after_save :increment_likes_counter
 
   def increment_likes_counter
-    post.increment!(:likes_counter)
+    post.update(likes_counter: post.likes.count)
   end
 end
