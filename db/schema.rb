22a4b6fa889422bcea9d0468e9a1d7ce6dbf9a1c
Olivier-Kango <olivierkango@gmail.com>
2023-01-31 19:31:31 -0800

    AddUserIdToPosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_175149) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_032956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_175149) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
