45f9cea615d7e572db2167a0f7b56f9e14f911c3
Olivier-Kango <olivierkango@gmail.com>
2023-01-31 19:31:59 -0800

    Create the RSpec test for Posts

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'post index action' do
+    before :each do
+      @user = User.create(name: 'Tom', photo: 'photo', bio: 'Teacher from Mexico.', posts_counter: 1)
+      get "/users/#{@user.id}/posts"
+    end
+
+    it 'should check if status was correct' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should check if correct index template is rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should check if response body includes correct placeholder text' do
+      expect(response.body).to include('This is the posts index page')
+    end
+  end
+
+  describe 'post show action' do
+    before :each do
+      @user = User.create(
+        name: 'Tom',
+        photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        bio: 'Teacher from Mexico.',
+        posts_counter: 1
+      )
+      @post = Post.create(
+        author: @user,
+        title: 'Hello',
+        text: 'This is my first post',
+        user_id: @user.id,
+        author_id: @user.id,
+        comments_counter: 0,
+        likes_counter: 0
+      )
+      get "/users/#{@user.id}/posts/#{@post.id}"
+    end
+
+    it 'should check if status was correct' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should check if correct index template is rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should check if response body includes correct placeholder text' do
+      expect(response.body).to include('This is the posts show page')
+    end
+  end
+end
