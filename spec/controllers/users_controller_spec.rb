6f87fd9f7a561305888fc8ba8be74c77efefcc15
Olivier-Kango <olivierkango@gmail.com>
2023-02-02 13:21:25 -0800

    Update the rspec test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,13 +18,13 @@ RSpec.describe 'Users', type: :request do
 
     # Check if the response body includes correct placeholder text.
     it 'should check if response body includes correct placeholder text' do
-      expect(response.body).to include('This is the Users index page')
+      expect(response.body).to include('Number of posts:')
     end
   end
 
   context 'user show action' do
     before :each do
-      @user = User.create(name: 'Tom', photo: 'photo', bio: 'Teacher from Mexico.', posts_counter: 1)
+      @user = User.create(name: 'Tom', photo: 'https://user-images.githubusercontent.com/108806646/216281530-fa5c3020-197a-498e-9024-339268ba1959.jpg', bio: 'Teacher from Mexico.', posts_counter: 1)
       get user_path(@user.id)
     end
 
@@ -40,7 +40,7 @@ RSpec.describe 'Users', type: :request do
 
     # Check if the response body includes correct placeholder text.
     it 'should check if response body includes correct placeholder text' do
-      expect(response.body).to include('This is the Users show page')
+      expect(response.body).to include('Bio')
     end
   end
 end
