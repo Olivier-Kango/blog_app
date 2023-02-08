ccc4113c1aa867847399a305e7656455958405e1
Olivier-Kango <olivierkango@gmail.com>
2023-02-08 14:33:46 -0800

    Fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_192110) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_003228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,16 +57,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_192110) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "confirmation_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "posts"
+  add_foreign_key "comments", "users", column: "post_id"
   add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "likes", "users", column: "post_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
