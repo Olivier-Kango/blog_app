5f60452388d451bb58212eaf97924d83a11bcc9f
Olivier-Kango <olivierkango@gmail.com>
2023-02-07 18:22:48 -0800

    Add user_signed_in?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    @users = User.all
+    if user_signed_in?
+      @users = User.all
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
-    @user = User.find(params[:id])
+    if user_signed_in?
+      @user = User.find(params[:id])
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
