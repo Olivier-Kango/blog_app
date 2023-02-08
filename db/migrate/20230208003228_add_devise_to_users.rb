c678be535cce03c7fc39fb65e16897ef652b72d1
Olivier-Kango <olivierkango@gmail.com>
2023-02-07 20:11:25 -0800

    Add some stylings

--- db/migrate/20230208003228_add_devise_to_users.rb
+++ db/migrate/20230208003228_add_devise_to_users.rb
@@ -34,10 +34,10 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
 
 
       # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+      t.timestamps null: false
     end
 
-    # add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
