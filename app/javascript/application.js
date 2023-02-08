727d43239f140ff74537acde9fb6ff3d19be22ef
Olivier-Kango <olivierkango@gmail.com>
2023-02-07 23:40:38 -0800

    Arrange the layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.querySelector('.reload').addEventListener('click', function () {
+  window.location.reload();
+});
