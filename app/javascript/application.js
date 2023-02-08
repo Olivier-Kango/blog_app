1a5c40f558f92cdbe0b45a6225fddeafb82e7351
Olivier-Kango <olivierkango@gmail.com>
2023-02-07 23:54:08 -0800

    Remove js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-document.querySelector('.reload').addEventListener('click', function () {
-  window.location.reload();
-});
