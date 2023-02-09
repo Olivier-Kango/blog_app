f43fc83df46ca64c98b7bf1378dc211492315842
Olivier-Kango <olivierkango@gmail.com>
2023-02-09 00:55:17 -0800

    Fix bugs for likes and comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,10 @@
 class LikesController < ApplicationController
-  def new
-    @like = Like.new
-  end
-
   def create
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @like = Like.new(user: @user, post_id: params[:post_id])
+    @like = Like.new(author: @user, post_id: params[:post_id])
 
-    if @like.save
-      redirect_to user_post_path(@user, @post)
-    else
-      render json: { error: @like.errors.full_messages.to_sentence }, status: :unprocessable_entity
-    end
+    redirect_to user_post_path(@user, @post) if @like.save
   end
 
   private
