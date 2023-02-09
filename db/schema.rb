e5016f21679ae3efcc7c4b802e203c81911df272
Olivier-Kango <olivierkango@gmail.com>
2023-02-09 03:08:28 -0800

    Add login and logout button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_075420) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_003228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,9 +65,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_075420) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "posts"
+  add_foreign_key "comments", "users", column: "post_id"
   add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "likes", "users", column: "post_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
